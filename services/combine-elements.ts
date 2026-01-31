a6d0e56330862cf003fc1c771f910c22ba67e9d7
Yousef Mahmoud <usef.mahmoud@outlook.com>
2026-01-31 16:03:36 +0200

    fix: update fetch URL in combineElements service to include 'api' prefix

--- services/combine-elements.ts
+++ services/combine-elements.ts
@@ -1,5 +1,5 @@
 export const combineElements = async (word1: string, word2: string) => {
-  return await fetch(`combine?word1=${word1}&word2=${word2}`, {
+  return await fetch(`api/combine?word1=${word1}&word2=${word2}`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
